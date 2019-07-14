38311df5f0f96327a812b2e65dda70e406831ed6
Yuki Hirai <yukihirai0505@gmail.com>
2019-07-14 21:15:05 +0900

    Add string List

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,13 +28,17 @@ class _MyAppState extends State<MyApp> {
                 onPressed: () {},
               ),
             ),
-            Card(
-              child: Column(
-                children: <Widget>[
-                  Image.asset('assets/food.jpg'),
-                  Text('Food Paradise')
-                ],
-              ),
+            Column(
+              children: _products
+                  .map((element) => Card(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset('assets/food.jpg'),
+                            Text(element)
+                          ],
+                        ),
+                      ))
+                  .toList(),
             ),
           ],
         ),
