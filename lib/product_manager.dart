2026da40c686ecf14207fd360b79c3209d2a5165
Yuki Hirai <yukihirai0505@gmail.com>
2019-07-17 21:35:02 +0900

    Understanding Lifecycle Hooks
    [Stateless Widget]
    
    - Constructor
    - build()
    
    [Stateful Widget]
    
    - Constructor
    - initState()
    - build()
    - setState()
    - didUpdateWidget()

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -3,24 +3,35 @@ import 'package:flutter_sample/products.dart';
 
 class ProductManager extends StatefulWidget {
   final String startingProduct;
-  ProductManager(this.startingProduct);
+  ProductManager(this.startingProduct) {
+    print('[ProductManager Widget] Constructor');
+  }
   @override
   State<StatefulWidget> createState() {
-    return _ProductManager();
+    print('[ProductManager Widget] createState()');
+    return _ProductManagerState();
   }
 }
 
-class _ProductManager extends State<ProductManager> {
+class _ProductManagerState extends State<ProductManager> {
   List<String> _products = [];
 
   @override
   void initState() {
+    print('[ProductManager State] initState()');
     _products.add(widget.startingProduct);
     super.initState();
   }
 
   @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    print('[ProductManager State] didUpdateWidget()');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('[ProductManager State] build()');
     return Column(
       children: [
         Container(
