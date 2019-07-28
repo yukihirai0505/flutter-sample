e077bb95f42bd7e7ea835cfb0135f56ae2066723
Yuki Hirai <yukihirai0505@gmail.com>
2019-07-28 22:19:14 +0900

    Styling Text Fields

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -8,20 +8,50 @@ class ProductCreatePage extends StatefulWidget {
 }
 
 class _ProductCreatePageState extends State<ProductCreatePage> {
-  String titleValue = '';
+  String titleValue;
+  String descriptionValue;
+  double priceValue;
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        TextField(
-          onChanged: (String value) {
-            setState(() {
-              titleValue = value;
-            });
-          },
-        ),
-        Text(titleValue),
-      ],
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: Column(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Product Title',
+            ),
+            onChanged: (String value) {
+              setState(() {
+                titleValue = value;
+              });
+            },
+          ),
+          TextField(
+            maxLines: 4,
+            decoration: InputDecoration(
+              labelText: 'Product Description',
+            ),
+            onChanged: (String value) {
+              setState(() {
+                descriptionValue = value;
+              });
+            },
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: 'Product Price',
+            ),
+            onChanged: (String value) {
+              setState(() {
+                priceValue = double.parse(value);
+              });
+            },
+          ),
+        ],
+      ),
     );
   }
 }
